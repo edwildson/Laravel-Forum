3a3e51a97d599edec6b73e236e357e38518279cb
Edwildson Coelho Rodrigues <edwildson@hotmail.com>
2022-02-19 13:47:29 -0300

    sqlite config update

--- config/database.php
+++ config/database.php
@@ -38,8 +38,6 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
         'mysql' => [
